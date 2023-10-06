ba70b4e7f77887e01000950f67ab6ce655b92ce2
bl-Rose <smatilda332@gamil.com>
2023-10-05 22:59:07 -0700

    htrhre

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - a computer that prints the size of various types on the computer it is compiled and run on
+ * Return 0(success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+printf("Size of a char: %lu bytes(s)\n", (unsigned long)sizeof(a);
